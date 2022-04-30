57eed19f8e9ba8b1d6a41e6872eff9fda7670b52
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-30 18:11:10 +0900

    fix(tailwind.config): modify custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateColumns: {
+        // Simple 9 column grid
+        // 9: "repeat(16, minmax(0, 1fr))",
+        9: "repeat(9, 1fr)",
         // Simple 16 column grid
-        16: "repeat(16, minmax(0, 1fr))",
+        // 16: "repeat(16, minmax(0, 1fr))",
+        16: "repeat(16, 1fr)",
         // Simple 32 column grid
-        32: "repeat(32, minmax(0, 1fr))",
+        // 32: "repeat(32, minmax(0, 1fr))",
+        32: "repeat(32, 1fr)",
       },
     },
   },
