5728d91041fc29312b35d4d12f25fda6b1c0b02c
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-30 16:48:19 +0900

    feat(Cell.tsx): Implement Cell component

--- components/Cell.tsx
+++ components/Cell.tsx
@@ -1,6 +1,43 @@
-const Cell: React.FC = () => {
+type Props = {
+  content?: number;
+};
+
+const Cell: React.FC<Props> = ({ content }) => {
+  let contentStyle = "";
+  if (content === 1) {
+    contentStyle = "text-blue-700";
+  } else if (content === 2) {
+    contentStyle = "text-green-700";
+  } else if (content === 3) {
+    contentStyle = "text-red-700";
+  } else if (content === 4) {
+    contentStyle = "text-lime-700";
+  } else if (content === 5) {
+    contentStyle = "text-orange-700";
+  } else if (content === 6) {
+    contentStyle = "text-indigo-700";
+  } else if (content === 7) {
+    contentStyle = "text-pink-700";
+  } else if (content === 8) {
+    contentStyle = "text-stone-700";
+  }
+
   return (
-    <div className="w-7 h-7 bg-gray-300 border-2 border-l-gray-50 border-t-gray-50 border-r-gray-600 border-b-gray-600"></div>
+    <div className="w-7 h-7 bg-gray-300 box-border border-2 border-l-gray-50 border-t-gray-50 border-r-gray-600 border-b-gray-600">
+      <div className={`flex justify-center items-center font-black`}>
+        {(() => {
+          if (content === 0) {
+            return <div></div>;
+          } else if (content === -1) {
+            return (
+              <div className="w-full h-full bg-red-700 flex justify-center items-center">B</div>
+            );
+          } else {
+            return <div className={contentStyle}>{content}</div>;
+          }
+        })()}
+      </div>
+    </div>
   );
 };
 
