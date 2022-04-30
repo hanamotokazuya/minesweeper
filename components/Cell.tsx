b9edda399a11a5907f3f9b99145736f456f5202c
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-30 14:07:37 +0900

    feat(create Cell component):
    n

---
+++ components/Cell.tsx
@@ -0,0 +1,7 @@
+const Cell: React.FC = () => {
+  return (
+    <div className="w-7 h-7 bg-gray-300 border-2 border-l-gray-50 border-t-gray-50 border-r-gray-600 border-b-gray-600"></div>
+  );
+};
+
+export default Cell;
