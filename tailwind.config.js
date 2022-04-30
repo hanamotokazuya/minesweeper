ec10b6aa92a79f1b18f80b0d02f25f8fdae9e2e3
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-30 16:46:34 +0900

    feat(tailwindcss): append custom style (grid-cols-16, grid-cols-32)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        16: "repeat(16, minmax(0, 1fr))",
+        // Simple 32 column grid
+        32: "repeat(32, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
