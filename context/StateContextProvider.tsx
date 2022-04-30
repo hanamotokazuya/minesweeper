fb67925226ecf5af5eb9daa7d3857a8b428145b1
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-30 18:15:55 +0900

    feat(StateContextProvider.tsx): Append new state (flagMode, setFlagMode)

--- context/StateContextProvider.tsx
+++ context/StateContextProvider.tsx
@@ -8,13 +8,20 @@ interface Props {
 const StateContext = createContext(
   {} as {
     level: Level;
+    flagMode: boolean;
     setLevel: React.Dispatch<SetStateAction<Level>>;
+    setFlagMode: React.Dispatch<SetStateAction<boolean>>;
   }
 );
 
 export const StateContextProvider: React.FC<Props> = ({ children }) => {
   const [level, setLevel] = useState<Level>("Easy");
-  return <StateContext.Provider value={{ level, setLevel }}>{children}</StateContext.Provider>;
+  const [flagMode, setFlagMode] = useState<boolean>(false);
+  return (
+    <StateContext.Provider value={{ level, flagMode, setLevel, setFlagMode }}>
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
