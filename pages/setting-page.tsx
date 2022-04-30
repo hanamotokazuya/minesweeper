2ffc5bb5043acf61be3eb613fb1346bdc8b788b0
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-04-30 14:04:59 +0900

    feat(Implement Setting page):
    n

--- pages/setting-page.tsx
+++ pages/setting-page.tsx
@@ -1,9 +1,33 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
+import { useStateContext } from "../context/StateContextProvider";
 
 const SettingPage: React.FC = () => {
+  const { level, setLevel } = useStateContext();
   return (
     <Layout>
-      <div>Setting page</div>
+      <Link href="/">
+        <a className="text-3xl text-red-600 ml-10 mr-auto mb-10 pt-20">Back</a>
+      </Link>
+      <h2 className="text-2xl text-green-400 mb-10">Game Level</h2>
+      <button
+        className={`${level === "Easy" ? "text-red-400" : "text-gray-300"} text-xl mb-10`}
+        onClick={() => setLevel("Easy")}
+      >
+        Easy ( 9 x 9 10 mines )
+      </button>
+      <button
+        className={`${level === "Normal" ? "text-red-400" : "text-gray-300"} text-xl mb-10`}
+        onClick={() => setLevel("Normal")}
+      >
+        Normal ( 16 x 16 40 mines )
+      </button>
+      <button
+        className={`${level === "Hard" ? "text-red-400" : "text-gray-300"} text-xl mb-10`}
+        onClick={() => setLevel("Hard")}
+      >
+        Hard ( 32 x 32 199 mines )
+      </button>
     </Layout>
   );
 };
