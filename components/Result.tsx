ca5be334e45ba0aaed36c500d5ca93fc7b6ac35e
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-01 22:31:41 +0900

    style(main-page): changed height of nav

--- components/Result.tsx
+++ components/Result.tsx
@@ -2,7 +2,7 @@ import { useStateContext } from "../context/StateContextProvider";
 
 const Result: React.FC = () => {
   const {
-    state: { progress },
+    state: { progress, time },
     action,
   } = useStateContext();
   return (
@@ -14,7 +14,8 @@ const Result: React.FC = () => {
         >
           <div className="w-full h-full bg-gray-500 opacity-50"></div>
           <div className="min-w-max absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 rounded-3xl bg-black p-8 sm:p-15">
-            <div className="text-2xl sm:text-3xl text-green-500">GAME CLEAR!!!</div>
+            <div className="text-2xl sm:text-3xl text-green-500 text-center">GAME CLEAR!!!</div>
+            <div className="sm:text-lg text-gray-300 text-center">Time: {time} [sec]</div>
           </div>
         </div>
       )}
@@ -25,7 +26,7 @@ const Result: React.FC = () => {
         >
           <div className="w-full h-full bg-gray-500 opacity-50"></div>
           <div className="min-w-max absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 rounded-3xl bg-black p-8 sm:p-15">
-            <div className="text-2xl sm:text-3xl text-red-600">GAME OVER!!!</div>
+            <div className="text-2xl sm:text-3xl text-red-600 text-center">GAME OVER!!!</div>
           </div>
         </div>
       )}
