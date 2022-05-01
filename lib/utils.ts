1c8a32f62e070372d65d3af1c578bf4dc5702398
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-01 18:22:44 +0900

    feat(utils): Extend randomIntArrayNoDuplication function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,25 @@
 export const range = (begin: number, end: number) =>
   [...Array(end - begin)].map((_, i) => begin + i);
 
-export const randomIntArrayNoDuplication = (maxInt: number, arrayNum: number) => {
-  if (maxInt < arrayNum) throw new Error("maxInt is bigger than arrayNum.");
+export const randomIntArrayNoDuplication = (
+  maxInt: number,
+  arrayNum: number,
+  exceptNum: number[] = []
+) => {
+  if (maxInt - exceptNum.length < arrayNum) {
+    throw new Error("(maxInt - exceptNum.length) is bigger than arrayNum.");
+  }
   let sequenceArray = range(0, maxInt);
+  if (exceptNum.length > 0) {
+    let sortedExceptNum = exceptNum.sort((a, b) => b - a);
+    if (sortedExceptNum[0] > maxInt - 1) {
+      throw new Error("exceptNum elements are smaller than maxInt.");
+    }
+    for (let i of sortedExceptNum) {
+      sequenceArray.splice(i, 1);
+    }
+  }
+
   let randomIntArray: number[] = [];
   for (let i = 0; i < arrayNum; i++) {
     let n = sequenceArray.length;
