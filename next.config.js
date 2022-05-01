c9bff95fae68b7cf47f9190fa13c1034bd817b18
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-01 15:27:15 +0900

    perf(next.config): Set to return home after redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/setting-page",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/main-page",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/help-page",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
