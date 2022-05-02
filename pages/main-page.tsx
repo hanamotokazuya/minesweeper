52b0e1917dd468de3074a36d1de33590e8826d81
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-02 13:09:33 +0900

    feat(main-page): Added images

--- pages/main-page.tsx
+++ pages/main-page.tsx
@@ -1,9 +1,10 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 import Link from "next/link";
 import GameField from "../components/GameField";
 import { useStateContext } from "../context/StateContextProvider";
 import Result from "../components/Result";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useInterval } from "../lib/useInterval";
 import { useHotkeys } from "react-hotkeys-hook";
 
@@ -26,6 +27,7 @@ const MainPage: React.FC = () => {
     autostart: false,
     onUpdate: () => action({ type: "TIMER_COUNT_EVENT" }),
   });
+  const [faceImage, setFaceImage] = useState("/niconico.png");
   useEffect(() => {
     if (remainingCells === mines && progress === "START") {
       action({ type: "GAMECLEAR_EVENT" });
@@ -39,6 +41,11 @@ const MainPage: React.FC = () => {
       timerControl.stop();
     }
   });
+  useEffect(() => {
+    if (progress === "READY") setFaceImage("/niconico.png");
+    else if (progress === "GAMECLEAR") setFaceImage("/glass.png");
+    else if (progress === "GAMEOVER") setFaceImage("/puapua.png");
+  }, [progress]);
 
   return (
     <Layout>
@@ -56,7 +63,9 @@ const MainPage: React.FC = () => {
             className="w-11 h-11 text-3xl flex justify-center items-center
               bg-gray-300 border-4 border-l-gray-50 border-t-gray-50 border-r-gray-600 border-b-gray-600"
             onClick={() => action({ type: "REFLESH_GAME_EVENT" })}
-          ></div>
+          >
+            <Image src={faceImage} alt="face" width={44} height={44} />
+          </div>
           <div className="bg-black flex justify-center items-center w-16 h-11">
             <span className="text-red-500 text-2xl">{String(time).padStart(4, "0")}</span>
           </div>
