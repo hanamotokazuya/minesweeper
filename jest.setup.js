874062401a592e890bc9ccb4b2b5816035a166bd
hanamotokazuya <hanamoto.kazuya@gmail.com>
2022-05-04 18:27:40 +0900

    fix(jest.setup): hide error messages about act() being unsupported in production build

--- jest.setup.js
+++ jest.setup.js
@@ -7,3 +7,11 @@ if (typeof global.TextEncoder === "undefined") {
 if (typeof global.TextDecoder === "undefined") {
   global.TextDecoder = require("util").TextDecoder;
 }
+// hide error messages about act() being unsupported in production build
+const ignoredErrors = [/act(...) is not supported in production builds of React./];
+const consoleError = global.console.error;
+global.console.error = (...args) => {
+  if (ignoredErrors.some((el) => el.test(args[0]))) {
+    return consoleError(...args);
+  }
+};
